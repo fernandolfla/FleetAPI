2e575c3a2e3a33dce2d6489659e5f98e969f4349
Luiz Fernando Lima Avelar <fernandolfla@gmail.com>
2024-08-08 09:08:41 -0300

    Build

--- Fleet/Models/Usuario.cs
+++ Fleet/Models/Usuario.cs
@@ -5,7 +5,7 @@ namespace Fleet.Models
     public class Usuario : DbEntity
     {
         public string Email { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty; 
         public PapelEnum Papel { get; set; }
     }
 }
